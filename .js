6958d2ea5410540f4b57a7ca9582ef6d4588ff38
bunnykumar <akshayk7375@gmail.com>
2022-01-07 22:36:32 +0530

    Initial commit

---
+++ .js
@@ -0,0 +1,26 @@
+var x = document.getElementById("i")
+var y = document.getElementById("my")
+function aksh() {
+
+    if (x.style.display != "none") {
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block";
+    }
+
+}
+
+// second fun
+var a = document.getElementById("n")
+
+function ab() {
+
+    if (a.style.display = "block") {
+        a.style.display = "none";
+    }
+
+}
+
+
+
